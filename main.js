9f0814c2c76e63c468974d4bffd74a32568d5407
Wendel Bueno <wendelbueno99@gmail.com>
2023-11-02 19:52:45 -0300

    adicionando JavaScript

---
+++ main.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />'
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />'
+
+let = linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    let = linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`
+    linha += '</tr>'
+
+    linhas += linha;
+
+    const corpotTabela = document.querySelector('tbody');
+    corpotTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+});
